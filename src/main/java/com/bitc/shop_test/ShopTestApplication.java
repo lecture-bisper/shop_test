0c601ae550f7fbfd4c846fab2e2db12864695ab7
bisper <bisper@outlook.kr>
2022-05-18 18:15:32 +0900

    프로젝트 등록.

---
+++ src/main/java/com/bitc/shop_test/ShopTestApplication.java
@@ -0,0 +1,13 @@
+package com.bitc.shop_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bitc/shop_test/ShopTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitc.shop_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
